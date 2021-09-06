534b83d03d3073d550dec9e94354659f33061365
Joaquin <joacovillamediana@gmail.com>
2021-09-05 21:05:15 -0300

    Web terminada

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+
+
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message'])) {
+  $name = $_POST['name'];
+  $email =  $_POST['email'];
+  $mes_subject = $_POST['subject'];
+  $message =  $_POST['message'];
+
+
+  $to =  'joacovillamediana@gmail.com';
+  $subject = "Recibiste un contacto";
+  $html = '
+  <html>
+  <body>
+  <p>
+  <u>Datos del contacto</u>
+  <br><br>Nombre: <b>' . $name . '</b>
+  <br><br>Email: <b>' . $email .  '</b>
+  <br><br>Asunto: <b>' . $mes_subject .  '</b>
+  <br><br>Mensaje: <b>' . $message .  '</b>                      
+
+                          </p>
+                      </body>
+                      </html>
+                      ';
+
+  // Always set content-type when sending HTML email
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+  // More headers
+  $headers .= 'From: <noreply@parodiyasociados.com.ar>' . "\r\n";
+
+  mail($to, $subject, $html, $headers);
+}
+
+header('Location: index.php?status=ok#contact');
+die;
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,26 +16,41 @@
 </head>
 
 <body>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/6133ae5ed6e7610a49b3a792/1feou28j6';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#"><img src="./assets/img/logo.png" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">NOSOTROS</a>
+                        <a class="nav-link" href="#about-us">NOSOTROS</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Especializaciones</a>
+                        <a class="nav-link" href="#specializations">Especializaciones</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Ciudadanias</a>
+                        <a class="nav-link" href="#citizenship">Ciudadanias</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contacto</a>
+                        <a class="nav-link" href="#contact">Contacto</a>
                     </li>
                 </ul>
 
@@ -48,12 +64,12 @@
         <div class="container">
             <h1 data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">Estudio Jurídico Parodi & Asoc</h1>
             <h5 class="desc" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">se encuentra formado por profesionales especializados en cada rama del derecho, con una constante capacitación.</h5>
-            <a href="" class="btn btn-contact" data-aos="fade-down" data-aos-delay="300" data-aos-duration="1000">Contactate con nosotros</a>
+            <a href="" id="contactnow" class="btn btn-contact" data-aos="fade-down" data-aos-delay="300" data-aos-duration="1000">Contactate con nosotros</a>
         </div>
         <img src="./assets/img/banner.jpg" alt="" class="background">
     </section>
 
-    <section class="about-us">
+    <section class="about-us" id="about-us">
         <div class="container">
             <h2 data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">Sobre Nosotros</h2>
             <div class="row row-about">
@@ -73,11 +89,11 @@
         </div>
     </section>
 
-    <section class="areas">
+    <section class="areas" id="specializations">
         <div class="container">
             <h2 class="mb-5" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">Especializaciones</h2>
             <div class="row">
-                <div class="col-md-4 col-item" data-aos="fade-down" data-aos-delay="200" data-aos-duration="1000">
+                <div class="col-lg-4 col-item" data-aos="fade-down" data-aos-delay="200" data-aos-duration="1000">
                     <div class="content">
                         <div class="box">
                             <i class="fas fa-house-user"></i>
@@ -108,7 +124,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-4 col-item" data-aos="fade-down" data-aos-delay="300" data-aos-duration="1000">
+                <div class="col-lg-4 col-item" data-aos="fade-down" data-aos-delay="300" data-aos-duration="1000">
                     <div class="content">
                         <div class="box">
                             <i class="fas fa-car-crash"></i>
@@ -127,7 +143,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-4 col-item" data-aos="fade-down" data-aos-delay="400" data-aos-duration="1000">
+                <div class="col-lg-4 col-item" data-aos="fade-down" data-aos-delay="400" data-aos-duration="1000">
                     <div class="content">
                         <div class="box">
                             <i class="fas fa-hard-hat"></i>
@@ -158,7 +174,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-4 col-item" data-aos="fade-down" data-aos-delay="500" data-aos-duration="1000">
+                <div class="col-lg-4 col-item" data-aos="fade-down" data-aos-delay="500" data-aos-duration="1000">
                     <div class="content">
                         <div class="box">
                             <i class="fas fa-file-alt"></i>
@@ -181,7 +197,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-4 col-item" data-aos="fade-down" data-aos-delay="600" data-aos-duration="1000">
+                <div class="col-lg-4 col-item" data-aos="fade-down" data-aos-delay="600" data-aos-duration="1000">
                     <div class="content">
                         <div class="box">
                             <i class="fas fa-gavel"></i>
@@ -219,13 +235,13 @@
         </div>
     </section>
 
-    <section class="speciality spain">
+    <section class="speciality spain" id="citizenship">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-lg-6">
                     <img src="./assets/img/sam-williams-UuGAw6nF0Vw-unsplash.jpg" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000" class="img-fluid" alt="">
                 </div>
-                <div class="col-md-6 col-text" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+                <div class="col-lg-6 col-text" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
                     <h3>NACIONALIDAD ESPAÑOLA</h3>
                     <p class="desc">Si tus raíces son españolas, existe la posibilidad que puedas solicitar la nacionalidad española Ya sea por descendencia, por matrimonio o incluso por residencia legal en España gracias a los acuerdos internacionales entre países.
                         <br><br> Si tu sueño es vivir en España, también te asesoramos con los trámites para que puedas vivir legalmente allá. Analizamos todas las posibilidades de acuerdo a la normativa vigente, para que puedas optar por la mejor opción,
@@ -240,13 +256,13 @@
         <div class="container">
             <div class="row">
 
-                <div class="col-md-6 col-text" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+                <div class="col-lg-6 col-text" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
                     <h3>CIUDADANÍA ITALIANA</h3>
                     <p class="desc">La característica principal de la forma de transmisión de la ciudadanía italiana es que se transmite de padre a hijo sin límite de generación, pero también sin cortes. <br><br> Estudiamos tu árbol genealógico para que puedas solicitar
                         el reconocimiento de la ciudadanía, te asesoramos y también reconstruimos y armamos tu carpeta consular mediante la solicitud y traducción de todos los documentos: actas de nacimiento, matrimonio y defunción. Tramitamos el Certificado
                         de No naturalización ante la Cámara Nacional Electoral y también solicitamos la documentación de tu AVO a Italia.</p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-lg-6">
                     <img data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000" src="./assets/img/mauricio-artieda-h7WxFNO3MNY-unsplash.jpg" class="img-fluid" alt="">
                 </div>
             </div>
@@ -254,12 +270,12 @@
         </div>
     </section>
 
-    <section class="contact">
+    <section class="contact" id="contact">
         <div class="container">
 
             <div class="row">
-                <div class="col-md-8">
-                    <form action="" method="post">
+                <div class="col-lg-8">
+                    <form action="contact.php" id="contactForm" method="post">
                         <h2 class="text-left" data-aos="fade-down" data-aos-delay="000" data-aos-duration="1000">
                             Contactenos</h2>
                         <div class="form-group mt-3" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
@@ -279,33 +295,32 @@
                             <textarea class="form-control" placeholder="Mensaje" maxlength="255" name="message" id="" cols="30" rows="10"></textarea>
                         </div>
 
-                        <button class="btn btn-submit" data-aos="fade-down" data-aos-delay="400" data-aos-duration="1000" type="button">Enviar</button>
+                        <button class="btn btn-submit" data-aos="fade-down" data-aos-offset="-500" data-aos-delay="400" data-aos-duration="1000" type="button">Enviar</button>
+
+                        <?php if ($_GET['status'] == 'ok') { ?>
+                            <span class="text-success d-block mt-2">Mensaje enviado con exito!</span>
+                        <?php } ?>
                     </form>
                 </div>
-                <div class="col-md-4 col-info" data-aos="fade-down" data-aos-delay="300" data-aos-duration="1000">
+                <div class="col-lg-4 col-info" data-aos="fade-down" data-aos-delay="300" data-aos-duration="1000">
                     <div class="content">
                         <div class="item">
-                            <i class="fas fa-phone-square-alt"></i> <label for=""><a target="_blank"
-                                    href="tel:+5491153493183">+54 9 11
+                            <i class="fas fa-phone-square-alt"></i> <label for=""><a target="_blank" href="tel:+5491153493183">+54 9 11
                                     5349-3183</a></label>
                         </div>
                         <div class="item">
-                            <i class="fas fa-envelope"></i> <label for=""><a target="_blank"
-                                    href="mailto:aparodi@parodiyasociados.com.ar">aparodi@parodiyasociados.com.ar</a></label>
+                            <i class="fas fa-envelope"></i> <label for=""><a target="_blank" href="mailto:aparodi@parodiyasociados.com.ar">aparodi@parodiyasociados.com.ar</a></label>
                         </div>
                         <div class="item">
-                            <i class="fab fa-instagram"></i> <label for=""><a target="_blank"
-                                    href="https://www.instagram.com/estudiojuridicoparodiyasoc/">estudiojuridicoparodiyasoc</a></label>
+                            <i class="fab fa-instagram"></i> <label for=""><a target="_blank" href="https://www.instagram.com/estudiojuridicoparodiyasoc/">estudiojuridicoparodiyasoc</a></label>
                         </div>
                         <div class="item">
-                            <i class="fab fa-facebook-square"></i><label for=""><a target="_blank"
-                                    href="https://www.facebook.com/estudiojuridicoparodiyasociados">ESTUDIO
+                            <i class="fab fa-facebook-square"></i><label for=""><a target="_blank" href="https://www.facebook.com/estudiojuridicoparodiyasociados">ESTUDIO
                                     JURIDICO PARODI &amp;
                                     ASOC</a></label>
                         </div>
                         <div class="item">
-                            <i class="fab fa-twitter-square"></i><label for=""><a target="_blank"
-                                    href="https://www.instagram.com/estudiojuridicoparodiyasoc/">ParodiEstudio</a></label>
+                            <i class="fab fa-twitter-square"></i><label for=""><a target="_blank" href="https://www.instagram.com/estudiojuridicoparodiyasoc/">ParodiEstudio</a></label>
                         </div>
                     </div>
                 </div>
@@ -364,7 +379,7 @@
         }
 
         if (!form_errors) {
-            //SUBMIT
+            $('#contactForm').submit();
         }
     });
 
@@ -382,6 +397,42 @@
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
+
+    $('.nav-link').click(function() {
+        let hash = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 50
+        }, 800, function() {});
+    });
+
+    $('#contactnow').click(() => {
+        event.preventDefault();
+        let hash = '#contact';
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 50
+        }, 800, function() {});
+    })
+
+    $(document).ready(function() {
+        $(this).scrollTop(0);
+        if (window.location.hash !== "") {
+
+            var hash = window.location.hash;
+            console.log(hash);
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 50
+            }, 800, function() {
+
+
+                window.location.hash = hash;
+            });
+        }
+
+
+
+
+    })
 </script>
 
 
